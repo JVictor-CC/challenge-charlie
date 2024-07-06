6473e48c0c70dfd22910b9d42aa1817bc86e542e
João Victor de Souza <jvcunha1221@hotmail.com>
2024-07-05 23:32:17 -0300

    build: add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,13 @@ module.exports = {
     entry: "./src/app.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 9000,
     },
     module: {
         rules: [
