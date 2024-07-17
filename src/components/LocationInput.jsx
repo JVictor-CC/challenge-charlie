e9c4320af3d8d000fab428f20edbeb6d5ba7a3de
João Victor de Souza <jvcunha1221@hotmail.com>
2024-07-16 22:35:05 -0300

    feat: add Navbar and Location Input

---
+++ src/components/LocationInput.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { useWeatherContext } from "../context/weatherContext";
+
+const LocationInput = () => {
+    const { setLocation, location } = useWeatherContext();
+    return (
+        <div className="w-8/12 justify-center items-center text-slate-400 bg-white overflow-hidden shadow-2xl rounded gap-2 px-2 hidden sm:flex">
+            <HiMagnifyingGlass className="text-2xl" />
+            <label htmlFor="location" className="sr-only">
+                Digite a localização desejada.
+            </label>
+            <input
+                type="text"
+                className="h-9 w-full leading-none outline-none text-lg"
+                id="location"
+                placeholder="Digite a localização desejada..."
+                aria-label="Localizar"
+                value=""
+            />
+        </div>
+    );
+};
+
+export default LocationInput;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import LocationInput from "./LocationInput";
+import UnitSwitch from "./UnitSwitch";
+
+const Navbar = () => {
+    return (
+        <nav className="w-full min-h-14 bg-black bg-opacity-60 text-white px-10 flex justify-between items-center absolute z-10">
+            <img className="h-10" src={logo} alt="Weather Web logo" />
+            <LocationInput />
+        </nav>
+    );
+};
+
+export default Navbar;
